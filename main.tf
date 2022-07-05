33f92a113704e950ac6247b1644d3985e6485f40
rajesh <ykrajesh@outlook.com>
2022-07-05 22:33:10 +0530

    Without add staging are commit the file

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
 provider "azurerm" {
   features {}
 }
+# create the resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
