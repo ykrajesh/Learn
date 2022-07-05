02c6a3a422a16c34f09e497a5875e3fd7ccd1404
rajesh <ykrajesh@outlook.com>
2022-07-05 22:43:57 +0530

    Without add staging are commit the file

--- main.tf
+++ main.tf
@@ -6,3 +6,10 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
+# create virtual network 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.Virtual_Machine_name[1]}-Vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
